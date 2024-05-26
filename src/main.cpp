af41f934d2048f7a71d248943e88459628660aa2
Mina Kim <mmmiiinn@me.com>
2024-05-26 14:58:01 +0200

    first

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <imgui.h>
+#include <imgui_impl_dx11.h>
+#include <imgui_impl_win32.h>
+#include <windows.h>
+#include <memory>
+#include <iostream>
+
+int	main()
+{
+	std::cout << "test" << std::endl;
+	return (EXIT_SUCCESS);
+}
